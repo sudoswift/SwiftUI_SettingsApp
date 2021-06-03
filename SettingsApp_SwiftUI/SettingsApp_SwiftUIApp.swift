e44ea84d54fc4147f9344059879613ae01ebca18
sudoswift <sudoswift@gmail.com>
2021-06-03 17:13:06 +0900

    Initial Commit

---
+++ SettingsApp_SwiftUI/SettingsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsApp_SwiftUIApp.swift
+//  SettingsApp_SwiftUI
+//
+//  Created by izvern on 2021/06/03.
+//
+
+import SwiftUI
+
+@main
+struct SettingsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
