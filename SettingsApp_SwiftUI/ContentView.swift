e8b0fb61f85a7a012ae883131b9309a6c461900d
sudoswift <sudoswift@gmail.com>
2021-06-04 16:22:20 +0900

    add try textfield

--- SettingsApp_SwiftUI/ContentView.swift
+++ SettingsApp_SwiftUI/ContentView.swift
@@ -10,24 +10,26 @@ import SwiftUI
 struct ContentView: View {
     
     @State var airplaneToggle = false
+    @State private var searchText = ""
     var body: some View {
         NavigationView{
-            List{
-                NavigationLink(
-                    destination: ProfileDetailView(),
-                    label: {
-                        MyProfileSection()
-                    })
-                SectionOne()
-                SectionTwo()
-                SectionThree()
-            } // Form or List
+                List{
+                    NavigationLink(
+                        destination: ProfileDetailView(),
+                        label: {
+                            MyProfileSection()
+                        })
+                    SectionOne()
+                    SectionTwo()
+                    SectionThree()
+                } // Form or List
             .listStyle(GroupedListStyle())
             .navigationTitle("설정")
+            }
         } // NavigationView
 
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
